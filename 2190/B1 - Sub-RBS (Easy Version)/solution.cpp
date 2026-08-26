b0c7253ad109f13a249023c98e3c7dc493c88a64
Puneet Kumar <puneetmishra2641@gmail.com>
2026-08-27 01:09:43 +0530

    Add Sub-RBS (Easy Version) [B1] from Codeforces

---
+++ 2190/B1 - Sub-RBS (Easy Version)/solution.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+ 
+ 
+    bool p=false;
+    int count=0;
+    for(auto it:s)
+    {
+        if(it==')') 
+        {
+            p=true;
+            continue;
+        }
+        if(p) count++;
+    }
+ 
+    if(count>=2) cout<<n-2<<endl;
+    else cout<<-1<<endl;
+}
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+}

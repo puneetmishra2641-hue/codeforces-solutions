e2e5d0c86b89a8c047df6a08ad259833c1697220
Puneet Kumar <puneetmishra2641@gmail.com>
2026-09-06 23:48:40 +0530

    Add farmpiggie and Subset Sum [A] from Codeforces

---
+++ 2246/A - farmpiggie and Subset Sum/solution.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+ 
+ 
+    for(int i=n ; i>=1 ; i--) cout<<i<<" ";
+    cout<<endl;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}

b7d70aa4181b3723460382c1aab18d3241dd410e
Puneet Kumar <puneetmishra2641@gmail.com>
2026-09-07 19:30:12 +0530

    Add Worms [B] from Codeforces

--- 474/B - Worms/solution.cpp
+++ 474/B - Worms/solution.cpp
@@ -4,36 +4,54 @@ using namespace std;
 void solve()
 {
     int n;
-    cin >>n ;
+    cin >> n ;
     vector<int>a(n);
+ 
+    vector<int>ps(n+1);
+ 
     for(int i=0 ; i<n ; i++)
     {
-        cin >>a[i];
-    }
- 
-    vector<int>ans;
- 
-    
-    for(int i=0 ; i<n ; i++)
-    {
-        for(int j =0 ; j< a[i] ; j++)
-        {
-            ans.push_back(i+1);
-        }
+        cin >> a[i];
+        ps[i+1]=ps[i]+a[i];
     }
  
     int m;
-    cin >>m;
-    for(int i=0 ; i<m ;i++)
+    cin >> m ;
+ 
+    for(int i=0 ; i<m ; i++)
     {
         int temp;
         cin >> temp;
-        cout<<ans[temp-1]<<endl;
+ 
+        int start=0;
+        int end=n;
+        int ans=0;
+ 
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(ps[mid] < temp)
+            {
+                start=mid+1;
+                continue;
+            }
+ 
+            ans=mid;
+            end=mid-1;
+            
+        }
+ 
+        cout<<ans<<endl;
     }
+ 
+ 
 }
  
 int main()
 {
  
+     ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     solve();
+    return 0;
 }

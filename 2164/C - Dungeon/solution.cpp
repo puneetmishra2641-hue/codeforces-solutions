f40587b5a7a58653004cca5ade6a83d412d57902
Puneet Kumar <puneetmishra2641@gmail.com>
2026-09-09 20:47:56 +0530

    Add Dungeon [C] from Codeforces

--- 2164/C - Dungeon/solution.cpp
+++ 2164/C - Dungeon/solution.cpp
@@ -1,86 +1,78 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define int long long
  
 void solve()
 {
-    int n,m;
+    int n, m;
     cin >> n >> m;
  
-    vector<int>a(n),b(m),c(m);
-    vector<pair<int,int>>puneet;
-    priority_queue<int,vector<int>,greater<int>>pq;
+    vector<int> b(m), c(m);
+    vector<pair<int, int>> puneet;
+    priority_queue<int, vector<int>, greater<int>> pq;
  
-    for(int i=0 ; i<n ; i++)
+    for (int i = 0; i < n; i++)
     {
-        cin >> a[i];
-       pq.push(a[i]);
+        int te;
+        cin >> te;
+        pq.push(te);
     }
  
-    for(int i= 0 ; i<m ; i++)
+    for (int i = 0; i < m; i++)
     {
         cin >> b[i];
-        puneet.push_back({b[i],i});
+        puneet.push_back({b[i], i});
     }
  
-    for(int i=0 ; i< m ; i++)
+    for (int i = 0; i < m; i++)
     {
         cin >> c[i];
     }
  
-    sort(a.begin(),a.end());
-    sort(puneet.begin(),puneet.end());
+    sort(puneet.begin(), puneet.end());
  
-    int cnt=0;
-    int idx=0;
-    set<pair<int,int>>st;
+    int cnt = 0;
+    int idx = 0;
+    priority_queue<int>pq1;
  
-    while(cnt < m && !pq.empty())
+    while (cnt < m && !pq.empty())
     {
-        int temp=pq.top();
-        while(idx<m)
+        int temp = pq.top();
+        while (idx < m)
         {
-            if(puneet[idx].first <= temp)
+            if (puneet[idx].first <= temp)
             {
-                int i=puneet[idx].second;
-                st.insert({c[i],i});
+                int i = puneet[idx].second;
+               pq1.push(c[i]);
                 idx++;
             }
  
-            else break;
+            else
+                break;
         }
  
-        if(st.empty())
+        if (pq1.empty())
         {
             pq.pop();
             continue;
         }
  
         cnt++;
-        auto it=st.end();
-        it--;
-       if(it->first  > 0) pq.push(max(temp,it->first));
-       pq.pop();
-        st.erase(it);
-      
- 
+        int val=pq1.top();
+        if (val > 0)
+            pq.push(max(temp, val));
+        pq.pop();
+        pq1.pop();
     }
  
- 
- 
-    cout<<cnt<<endl;
- 
- 
- 
-    
- 
+    cout << cnt << endl;
 }
  
 signed main()
 {
     int t;
     cin >> t;
-    while(t--)
+    while (t--)
     {
         solve();
     }
